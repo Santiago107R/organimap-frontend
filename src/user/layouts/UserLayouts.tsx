f3f972d5061a1392a89dc0695f83bf35a3191a6d
Ssanti <santiagorobles107k@gmail.com>
2026-05-03 20:40:04 -0300

    react-router installed

--- src/user/layouts/UserLayouts.tsx
+++ src/user/layouts/UserLayouts.tsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router"
+import CustomFooter from "../components/CustomFooter"
+import CustomHeader from "../components/CustomHeader"
+import { UserCircle2 } from "lucide-react"
 
 
 const UserLayouts = () => {
   return (
     <>
-
+      <CustomHeader title={"Tienda"} link={[{ name: "Horarios", url: '/' }, { name: "Mapa", url: 'mapa' }, { name: "Lista", url: 'lista' },]} logo={'/favicon.svg'} perf={<UserCircle2 className='w-20 h-20 rounded-full object-cover"' />} />
+        <Outlet />
+      <CustomFooter description="Derechos reservados por Organimap" link={[{ name: "Arturitorodriguezvera@gmail.com", url: "#" }, { name: "ezequielnicolasacosta@gmail.com", url: "#" }]} />
     </>
   )
 }
