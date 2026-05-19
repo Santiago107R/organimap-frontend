e3750e695fff20d66f503a2a3513149e4c7bb330
ezee131415 <ezequielnicolasacosta@gmail.com>
2026-05-19 02:29:20 -0300

    cambio en el la classroomcard para que pueda interactuar bien y abrirse con la ventana desplegable(classroomModel:cambiales el nombre si quieres al los componentes

--- src/user/components/ClassroomCard.tsx
+++ src/user/components/ClassroomCard.tsx
@@ -1,52 +1,83 @@
+import { useState } from "react"
+import ClassroomModal from "./ClassroomModal"
+
 interface Props {
     shadow?: boolean;
     title: string;
     img: string;
     capacidad: string;
-    disp: "Disponible" | "Ocupada" | "Deshabilitada";
+
+    disp: "Disponible" | "Ocupada" | "Mantenimiento";
+
+    profesor?: string;
+    materia?: string;
+    descripcion?: string;
 }
 
-const ClassroomCard = ({ shadow, title, img, capacidad, disp }: Props) => {
+const ClassroomCard = ({shadow, title, img, capacidad, disp, profesor, materia, descripcion }: Props) => {
+
+    const [open, setOpen] = useState(false)
+
     const statusColors = {
         Disponible: "text-green-500",
         Ocupada: "text-red-500",
-        Deshabilitada: "text-gray-500",
+        Mantenimiento: "text-gray-500",
     }
+
     return (
+        <>
 
-        <div className={` flex flex-col py-8 px-3 bg-[#D9D9D9] rounded-2xl w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl ${shadow && 'shadow-[10px_10px_7px_rgba(0,0,0,0.25)]'}`}>
-            <div className="flex  gap-6 sm:gap-8 md:gap-4 lg:gap-10  ml-8  ">
-                <img
-                    src={img}
-                    alt="Imagen"
-                    className="  shrink-0 object-cover mt-1  rounded-sm bg-black w-19 h-15 sm:w-22 sm:h-17 md:w-22 md:h-16"
-                />
+            <div
+            onClick={() => setOpen(true)}
+            className={` flex flex-col py-8 px-3 bg-[#D9D9D9] rounded-2xl w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl cursor-pointer transition hover:scale-105 ${shadow && 'shadow-[10px_10px_7px_rgba(0,0,0,0.25)]'} `}
+            >
 
-                <h2 className="text-2xl  sm:text-2xl md:text-2xl lg:text-3xl font-anton min-h-[2.5em] ">
-                    {title}
-                </h2>
-            </div>
+                <div className="flex gap-6 sm:gap-8 md:gap-4 lg:gap-10 ml-8">
 
-            <div className="flex flex-col font-anybody ml-15  mt-4">
-                <ul className=" text-sm sm:text-md md:text-lg">
-                    <li>
-                        <span className="font-bold">Capacidad:  </span>
+                    <img
+                        src={img}
+                        alt="Imagen"
+                        className="shrink-0 object-cover mt-1 rounded-sm bg-black w-19 h-15 sm:w-22 sm:h-17 md:w-22 md:h-16"
+                    />
 
-                        {capacidad}
-                    </li>
-                    <li>
-                        <p className="font-bold">
-                            <span >Estado:</span>
+                    <h2 className="text-2xl sm:text-2xl md:text-2xl lg:text-3xl font-anton min-h-[2.5em]">
+                        {title}
+                    </h2>
 
-                            <span className={statusColors[disp]}>
-                                {disp}
+                </div>
+
+                <div className="flex flex-col font-anybody ml-15 mt-4">
+                    <ul className="text-sm sm:text-md md:text-lg">
+                        <li>
+                            <span className="font-bold">
+                                Capacidad:
                             </span>
-                        </p>
-                    </li>
-                </ul>
 
+                            {" "}{capacidad}
+                        </li>
+
+                        <li>
+                            <p className="font-bold">
+                                <span>
+                                    Estado:
+                                </span>
+
+                                {" "}
+
+                                <span className={statusColors[disp]}>
+                                    {disp}
+                                </span>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div >
+            {
+                open && (
+                    <ClassroomModal title={title} capacidad={capacidad} disp={disp} profesor={profesor} materia={materia} descripcion={descripcion} onClose={() => setOpen(false)} />
+                )
+            }
+        </>
     )
 }
 
--- src/user/pages/list/ListPage.tsx
+++ src/user/pages/list/ListPage.tsx
@@ -1,21 +1,51 @@
 import ClassroomCard from "@/user/components/ClassroomCard"
 
+const classrooms = [
+  {
+    id: 1,
+    title: "Sala grande",
+    img: "/favicon.svg",
+    capacidad: "40 estudiantes",
+    disp: "Disponible" as const,
+    profesor: "Juan",
+    materia: "Matemática",
+    descripcion: "Sala principal"
+  },
+
+  {
+    id: 2,
+    title: "Laboratorio de informática 1",
+    img: "/favicon.svg",
+    capacidad: "20 estudiantes",
+    disp: "Mantenimiento" as const,
+    profesor: "Carlos",
+    materia: "Programación",
+    descripcion: "No funcionan algunas PC"
+  },
+
+  {
+    id: 3,
+    title: "Aula 5",
+    img: "/favicon.svg",
+    capacidad: "20 estudiantes",
+    disp: "Ocupada" as const,
+    profesor: "Sossa",
+    materia: "Programación",
+    descripcion: "No andan 3 PC's"
+  }
+]
+
 const ListPage = () => {
   return (
     <div className="p-10 flex flex-col gap-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15">
-                <ClassroomCard title="Sala grande" img={'/favicon.svg'} capacidad="40 estudiantes" disp="Disponible" shadow />
-                <ClassroomCard title="Laboratorio de informatica 1" img={'/favicon.svg'} capacidad="20 estudiantes" disp="Deshabilitada" shadow />
-                <ClassroomCard title="Laboratorio de informatica 2" img={'/favicon.svg'} capacidad="32 estudiantes" disp="Ocupada" shadow />
-                <ClassroomCard title="Sala chica" img={'/favicon.svg'} capacidad="20 estudiantes" disp="Disponible" shadow />
-                <ClassroomCard title="Laboratorio 1" img={'/favicon.svg'} capacidad="25 estudiantes" disp="Deshabilitada" shadow />
-                <ClassroomCard title="Laboratorio 2" img={'/favicon.svg'} capacidad="15 estudiantes" disp="Ocupada" shadow />
-                <ClassroomCard title="Taller" img={'/favicon.svg'} capacidad="31 estudiantes" disp="Disponible" shadow />
-                <ClassroomCard title="Biblioteca" img={'/favicon.svg'} capacidad="28 estudiantes" disp="Deshabilitada" shadow />
-                <ClassroomCard title="Aula 5" img={'/favicon.svg'} capacidad="20 estudiantes" disp="Ocupada" shadow />
-                
-            </div>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15">
+
+        {classrooms.map((room) => (
+          <ClassroomCard key={room.id} title={room.title} img={room.img} capacidad={room.capacidad} disp={room.disp} profesor={room.profesor} materia={room.materia} descripcion={room.descripcion} shadow />
+        ))}
+        
+      </div>
+    </div>
   )
 }
 
